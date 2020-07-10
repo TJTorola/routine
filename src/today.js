df8598b14e2cec6074a75af633b85ab6b8593f78
Tyler Torola <Tyler@TJT.Codes>
2020-07-10 13:51:19 -0400

    Remove title bar from routes
    The nav bar covers that.

--- src/today.js
+++ src/today.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Divider, Layout, Text, TopNavigation } from '@ui-kitten/components';
+import { Layout, Text } from '@ui-kitten/components';
 
-export default ({ navigation }) => (
-  <>
-    <TopNavigation title='Today' alignment='center' />
-    <Divider/>
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text category='h1'>TODAY</Text>
-    </Layout>
-  </>
+export default () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>TODAY</Text>
+  </Layout>
 );
