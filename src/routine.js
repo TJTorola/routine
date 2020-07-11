40c01507b4e91329c8a1cd39ef30bd9ac5476db8
Tyler Torola <Tyler@TJT.Codes>
2020-07-11 17:54:31 -0400

    Flesh out Routine view
    Basically a TODO list at the moment, need to add some persistance layer
    next before moving deeper into this.

--- src/routine.js
+++ src/routine.js
@@ -1,8 +1,52 @@
 import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
+import { Button, Layout, List, ListItem, Text, Input, Divider } from '@ui-kitten/components';
 
-export default () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h1'>ROUTINE</Text>
-  </Layout>
-);
+class Routine extends React.Component {
+  state = {
+    tasks: [],
+    newTask: '',
+  }
+
+  taskId = 0
+
+  addTask = () => {
+    const task = this.state.newTask.trim();
+    if (task.length === 0) return;
+
+    const id = this.taskId++;
+
+    this.setState({ tasks: [...this.state.tasks, { task, id }], newTask: '' });
+  }
+
+  removeTask = id => () => {
+    // TODO make this ID specific
+    const tasks = this.state.tasks.filter(t => t.id !== id);
+    this.setState({ tasks });
+  }
+
+  setNewTask = newTask => this.setState({ newTask });
+
+  renderTaskRight = id => () => <Button onPress={this.removeTask(id)}>X</Button>
+
+  renderTask = ({ item }) => (
+    <ListItem key={item.id} title={item.task} accessoryRight={this.renderTaskRight(item.id)} />
+  )
+
+  render() {
+    return (
+      <>
+        <List
+          data={this.state.tasks}
+          renderItem={this.renderTask}
+          ItemSeparatorComponent={Divider}
+        />
+        <Layout style={{ flexDirection: 'row' }}>
+          <Input style={{ flex: 1 }} value={this.state.newTask} onChangeText={this.setNewTask} />
+          <Button onPress={this.addTask}>Add</Button>
+        </Layout>
+      </>
+    );
+  }
+}
+
+export default Routine;
